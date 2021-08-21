060cac274c8325757a67bd39ca3c9130f3b972dd
Nguyễn Mạnh Đạt <mydatng@gmail.com>
2021-08-21 18:19:30 +0700

    Complete module

---
+++ WishList/Models/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
